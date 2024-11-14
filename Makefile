2918a3d62ed7951c4325f5ca10d47f765c056e70
agustin <agsalgado2023@udec.cl>
2024-11-13 21:15:59 -0300

    añadida musica chistosa

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	gcc -o pacman src/main.c -IC:\SDL2\include -LC:\SDL2\lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image
+	gcc -o pacman src/main.c -IC:\SDL2\include -LC:\SDL2\lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lsdl2_mixer
--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
@@ -61,6 +62,32 @@ int main(int argc, char* argv[]) {
         SDL_Quit();
         return 1;
     }
+    //AGREGAR AUDIO
+    if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+        printf("No se pudo inicializar SDL: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    if (Mix_Init(MIX_INIT_MP3 | MIX_INIT_OGG) == 0) {
+        printf("No se pudo inicializar SDL_mixer: %s\n", Mix_GetError());
+        return -1;
+    }
+    
+    if (Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) < 0) {
+        printf("No se pudo abrir el audio: %s\n", Mix_GetError());
+        return -1;
+    }
+
+    Mix_Music *music = Mix_LoadMUS("audio/fondo.mp3");
+    if (!music) {
+        printf("No se pudo cargar la música: %s\n", Mix_GetError());
+        return -1;
+    }
+
+    Mix_PlayMusic(music, -1); // -1 para reproducción en loop infinito
+    Mix_VolumeMusic(1);
+
+
 
     // Bucle para mantener la ventana abierta
     int ejecutando = 1;
@@ -79,11 +106,14 @@ int main(int argc, char* argv[]) {
     }
 
     // Limpiar y cerrar SDL
+    Mix_FreeMusic(music);
+    Mix_CloseAudio();
     SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(ventana);
     SDL_Quit();
     IMG_Quit();
+    Mix_Quit();
     return 0;
 }
 
